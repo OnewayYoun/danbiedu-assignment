d44249c28c6e409e6fb66dc1c10af15df762cb14
OnewayYoun <onewayy91@gmail.com>
2023-06-16 20:24:00 +0900

    feat: initial setup for partial_update

--- danbiedu_assignment/tasks/views/tasks_view.py
+++ danbiedu_assignment/tasks/views/tasks_view.py
@@ -1,16 +1,26 @@
 from django.db.transaction import atomic
 from rest_framework.viewsets import GenericViewSet
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, mixins
 
 from tasks.serializers.tasks_serializer import TaskSerializer, SubTaskSerializer
 from tasks.models.tasks_model import Task, SubTask
+from tasks.permissions.tasks_permission import IsTaskOwner
 from users.models.users_model import User
 
 
-class TaskViewSet(GenericViewSet):
+class TaskViewSet(mixins.UpdateModelMixin, GenericViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+    permission_classes_by_action = dict(
+        partial_update=[IsTaskOwner],
+    )
+
+    def get_permissions(self):
+        permissions = self.permission_classes_by_action.get(
+            self.action, self.permission_classes
+        )
+        return [permission() for permission in permissions]
 
     @atomic
     def create(self, request, *args, **kwargs):
@@ -19,6 +29,13 @@ class TaskViewSet(GenericViewSet):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def partial_update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 class SubTaskViewSet(GenericViewSet):
